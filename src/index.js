914c72f211f7cce259c613456114b3031c8749bb
Gus <Chemchu@users.noreply.github.com>
2021-02-11 20:52:06 +0100

    Ara si

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './code/css/index.css';
+import App from './code/js/App';
+import reportWebVitals from './code/js/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
