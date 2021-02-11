08116ea58825ae8cfcf485b67df59d697ca80737
Gus <Chemchu@users.noreply.github.com>
2021-02-11 20:35:43 +0100

    Prueba 1

---
+++ src/code/js/App.js
@@ -0,0 +1,18 @@
+import logo from '../../svg/heartsplosion.svg';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ¡Bienvenida!
+        </p>
+          
+      </header>
+    </div>
+  );
+}
+
+export default App;
